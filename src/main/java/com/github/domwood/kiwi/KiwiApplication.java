79ab07eee43cddbde3312985471caad2c8d6bfc6
Dominic Wood <dominicwood1988@googlemail.com>
2019-02-09 09:11:05 +0000

    Initial Commit

---
+++ src/main/java/com/github/domwood/kiwi/KiwiApplication.java
@@ -0,0 +1,14 @@
+package com.github.domwood.kiwi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwiApplication.class, args);
+	}
+
+}
+
---
+++ src/main/ui/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './Kiwi2.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <br/>
+          KIWI!
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
