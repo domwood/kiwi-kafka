39140c9ec089f99eb15cd5bd9f5c408bdd3049b8
Dominic Wood <dominicwood1988@googlemail.com>
2022-04-09 10:41:14 +0100

    Change thread to be expandable

--- src/main/java/com/github/domwood/kiwi/kafka/task/KiwiTaskExecutor.java
+++ src/main/java/com/github/domwood/kiwi/kafka/task/KiwiTaskExecutor.java
@@ -3,24 +3,28 @@ package com.github.domwood.kiwi.kafka.task;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class KiwiTaskExecutor implements Executor {
-    private final ExecutorService delegate;
+    private final ThreadPoolExecutor delegate;
 
-    private KiwiTaskExecutor(ThreadFactory kiwiFactory){
-        delegate = Executors.newFixedThreadPool(50, kiwiFactory);
+    private KiwiTaskExecutor(final ThreadFactory kiwiFactory) {
+        delegate = new ThreadPoolExecutor(50, Integer.MAX_VALUE,
+                60L, TimeUnit.SECONDS,
+                new SynchronousQueue<>(),
+                kiwiFactory);
     }
 
-    private static class KiwiTaskExecutorHelper{
+    private static class KiwiTaskExecutorHelper {
         private static final KiwiTaskExecutor INSTANCE = new KiwiTaskExecutor(new ThreadFactoryBuilder()
                 .setNameFormat("kiwi-task-thread-%d")
                 .build());
     }
 
-    public static KiwiTaskExecutor getInstance(){
+    public static KiwiTaskExecutor getInstance() {
         return KiwiTaskExecutorHelper.INSTANCE;
     }
 
